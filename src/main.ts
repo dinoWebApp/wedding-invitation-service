7179df1037acfd7a513494f9997d60330107e287
nomadWebDeveloper <kgjsmart1559@gmail.com>
2024-07-16 21:10:57 +0900

    change port

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
